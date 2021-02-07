501a41b830fc06b523b36547aa834d81e3c6a7ba
Harry Fischer <harry.fischer@guardian.co.uk>
2021-02-07 14:31:43 +0000

    fixed config

---
+++ assets/scroll.js
@@ -0,0 +1,15 @@
+// function replaceVerticalScrollByHorizontal(event) {
+//   if (event.deltaY != 0) {
+//     // manually scroll horizonally instead
+//     window.scroll(window.scrollX + event.deltaY * 5, window.scrollY);
+
+//     // prevent vertical scroll
+//     event.preventDefault();
+//   }
+//   return;
+// }
+
+// window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
+
+
+// console.log("Scroll")
