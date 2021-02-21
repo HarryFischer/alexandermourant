648b916dc5707485b70ba85e752798415e71de22
Harry Fischer <harry.fischer@guardian.co.uk>
2021-02-21 18:19:20 +0000

    started nav

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,15 +1,24 @@
-// function replaceVerticalScrollByHorizontal(event) {
-//   if (event.deltaY != 0) {
-//     // manually scroll horizonally instead
-//     window.scroll(window.scrollX + event.deltaY * 5, window.scrollY);
+const scrollWrapper = document.querySelector('.page-content');
+const mq = window.matchMedia("(min-width: 600px)");
 
-//     // prevent vertical scroll
-//     event.preventDefault();
-//   }
-//   return;
-// }
+if (mq.matches) {
+    (function () {
+        function scrollHorizontally(e) {
+            e = window.event || e;
+            var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+            scrollWrapper.scrollLeft -= (delta * 10); // Multiplied by 40
+            e.preventDefault();
+        }
+        if (scrollWrapper.addEventListener) {
+            // IE9, Chrome, Safari, Opera
+            scrollWrapper.addEventListener("mousewheel", scrollHorizontally, false);
+            // Firefox
+            scrollWrapper.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+        } else {
+            // IE 6/7/8
+            scrollWrapper.attachEvent("onmousewheel", scrollHorizontally);
+        }
+    })();
+}
 
-// window.addEventListener('wheel', replaceVerticalScrollByHorizontal);
 
-
-// console.log("Scroll")
