3c5e9a9db55efba753ff73dce5742e7d757a3553
Harry Fischer <harry.fischer@guardian.co.uk>
2021-02-22 23:43:51 +0000

    add nav initial js

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+console.log("main working")
+
+const artworksBtn = document.querySelector('.artworks-title')
+const artworkList = document.querySelector('.hide-nav')
+
+artworksBtn.addEventListener('click', () => {
+    console.log("clicked")
+    artworkList.classList.toggle("show-nav")
+})
