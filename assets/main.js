911866e127e0fdef201eaa7fb3b5a4d8290a48a2
Harry Fischer <harry.fischer@guardian.co.uk>
2021-03-06 16:36:08 +0000

    start scroll animations

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,34 @@ artworksBtn.addEventListener('click', () => {
     console.log("clicked")
     artworkList.classList.toggle("show-nav")
 })
+
+
+
+
+
+
+// const images = document.querySelectorAll(".rollover-image")
+const images = document.querySelectorAll('.link-container')
+console.log(images)
+
+function elementInViewport(el) {
+    const windowWidth = window.innerWidth
+    console.log(windowWidth)
+    const elementScroll = el.getBoundingClientRect().right
+    // const elementScroll = el.getBoundingClientRect().right - windowWidth
+    console.log(elementScroll)
+    const offSet = 60
+    return elementScroll < (offSet * -1)
+}
+console.log(elementInViewport())
+console.log(elementScroll)
+
+document.addEventListener("scroll", () => {
+    images.forEach((i) => {
+        if (elementInViewport(i)) {
+            i.classList.add('inView')
+        } else {
+            i.classList.remove('inView')
+        }
+    })
+})
