55f41570f01fe1cfdda1a89e4dda8e6883b403c1
Harry Fischer <harry.fischer@guardian.co.uk>
2021-11-30 21:41:49 +0000

    updates

--- assets/main.js
+++ assets/main.js
@@ -57,35 +57,45 @@ if (iO) {
 
 
 
-// close farm left nav
-const leftPanel = document.querySelector(".split-wrapper__left")
-leftPanel.addEventListener(("click"), () => {
-  if (!leftPanel.classList.contains("closed")) {
-    leftPanel.classList.add ("closed")
-  } else {
-    leftPanel.classList.remove ("closed")
-  }
-})
+
 
 
 // fill olsar iframe on click
 const iframe = document.querySelector(".fill-me")
 const links = document.querySelectorAll(".olsar-link")
+const doc = iframe.contentDocument;
+console.log(iframe)
+console.log(doc)
+console.log(iframe.contentDocument.body.innerHTML)
 
-console.log(links)
+
+const addCss = () => {
+  iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<style>header {display:none;}</style>';
+}
 
 
 for (let i = 0; i < links.length; i++) {
-  console.log(links[i])
   links[i].addEventListener(("click"), () => {
-    console.log(links[i])
     iframe.src = (links[i].dataset.link)
     iframe.classList.add("filled")
     links[i].classList.add("active")
     if (!leftPanel.classList.contains("closed")) {
-      leftPanel.classList.add ("closed")
+      leftPanel.classList.add("closed")
+      iframe.style.display = 'block'
     } else {
-      leftPanel.classList.remove ("closed")
+      leftPanel.classList.remove("closed")
     }   
   })
 }
+
+// close farm left nav
+const leftPanel = document.querySelector(".split-wrapper__left")
+leftPanel.addEventListener(("click"), () => {
+  if (!leftPanel.classList.contains("closed")) {
+    leftPanel.classList.add("closed")
+    addCss()
+  } else {
+    leftPanel.classList.remove("closed")
+    iframe.style.display = 'none'
+  }
+})
